df92aa91bc8053df332c4dc6643d967b277ba079
Gambleruin <dannylovewargame@gmail.com>
2018-06-04 19:53:45 -0300

    *

--- ps1-warmup/src/warmup/Quadratic.java
+++ ps1-warmup/src/warmup/Quadratic.java
@@ -1,5 +1,6 @@
 package warmup;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Quadratic {
@@ -11,9 +12,31 @@ public class Quadratic {
      * @param c constant term.  Requires that a, b, and c are not ALL zero.
      * @return all integers x such that ax^2 + bx + c = 0.
      */
-    public static Set<Integer> roots(int a, int b, int c) {
-        throw new RuntimeException("not implemented yet;"); // TODO: delete this line when you implement it
+    public static Set<Double> roots(int a, int b, int c) {
+    	Set<Double> set = new HashSet<Double>();
+    	double t=b*b-4*a*c;  
+    	
+    	
+        if(t>0) {  
+            double r1=(Math.sqrt(t)-b)*1.0/(2*a);  
+            double r2=(-(Math.sqrt(t))-b)*1.0/(2*a); 
+            set.add(r1);
+            set.add(r2);
+        }  
+        else if(t==0)  {
+        	double r1 =(Math.sqrt(t)-b)*1.0/(2*a);
+        	set.add(r1);
+        	}
+        else  
+            System.out.println("No solution."); 
+        
+        
+        return set;
+        
+        
+    	
     }
+
 
     
     /**
@@ -22,7 +45,7 @@ public class Quadratic {
      */
     public static void main(String[] args) {
         System.out.println("For the equation x^2 - 4x + 3 = 0, the possible solutions are:");
-        Set<Integer> result = roots(1, -4, 3);
+        Set<Double> result = roots(1, -4, 3);
         System.out.println(result);
     }
 
